447938bc883857ec3bb9d5b0e07c0824801b40b9
luisee2019 <54379397+luisee2019@users.noreply.github.com>
2022-06-14 17:48:24 -0400

    Delete 01-Lab-code-luis-espinal-05-20-2022.py

--- 01-Lab-code-luis-espinal-05-20-2022.py
+++
@@ -1,7 +0,0 @@
-from tokenize import String
-
-# This program calculates the circumference of a circle
-pi = 3.14159
-diameter = 23
-circumference = pi * diameter
-print("The circumference of a " +str(diameter) + " diameter circle is "+str(circumference) + ".")
