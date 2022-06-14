f24249599109a87dc7f6600c64781f943fb4d58b
luisee2019 <54379397+luisee2019@users.noreply.github.com>
2022-06-14 17:50:04 -0400

    Delete unit01quiz.py

--- unit01quiz.py
+++
@@ -1,6 +0,0 @@
-sandwich_count = input("How many sandwiches are we making?)""\n")
-int(sandwich_count)
-flour_needed = int(sandwich_count) / 10
-water_needed = flour_needed * 66 / 100
-yeast_needed = 1.3
-print("The kilos of Acme's dry yeast needed are: " +str(yeast_needed))
